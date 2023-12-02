ddd739c09538d36fd485736743af3ba3068faa70
Renato Oliveira <renato.loliveira89@gmail.com>
2023-12-02 16:39:53 -0300

    adicionando rotas para as telas

---
+++ src/pages/passwords/index.js
@@ -0,0 +1,13 @@
+import {View, Text, StyleSheet, VirtualizedList} from 'react-native'
+
+export function Passwords() {
+    return(
+        <View>
+            <Text>Minhas senhas</Text>
+        </View>
+    )
+}
+
+const meuEstilo = StyleSheet.create({
+    
+})
