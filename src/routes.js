30a6fe2f0c6ba57efa5a6ec24f851e9a0a3a08c7
Renato Oliveira <renato.loliveira89@gmail.com>
2023-12-02 16:50:16 -0300

    adicionando ícones a nas abas das páginas

--- src/routes.js
+++ src/routes.js
@@ -2,13 +2,50 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { Home } from './pages/home'
 import { Passwords } from './pages/passwords'
 
+import { Ionicons } from '@expo/vector-icons'
+
 const Tab = createBottomTabNavigator();
 
 export function Routes() {
     return (
         <Tab.Navigator>
-            <Tab.Screen name='home' component={ Home }/>
-            <Tab.Screen name='passwords' component={ Passwords }/>
+            <Tab.Screen
+                name='home'
+                component={Home}
+                options={{
+                    //removendo o nome da barra
+                    tabBarShowLabel: false,
+                    // removendo o título da página
+                    headerShown: false,
+                    // inserindo ícone da barra
+                    tabBarIcon: ({ focused, size, color }) => {
+                        if(focused) {
+                            return <Ionicons size={size} color={color} name='home'/>
+                        }
+
+                        return <Ionicons size={size} color={color} name='home-outline'/>
+                    }
+                }}
+            />
+            <Tab.Screen
+                name='passwords'
+                component={Passwords}
+                // tirando o header da página
+                options={{
+                    //removendo o nome da barra
+                    tabBarShowLabel: false,
+                    // removendo o título da página
+                    headerShown: false,
+                    // inserindo ícone da barra
+                    tabBarIcon: ({ focused, size, color }) => {
+                        if(focused) {
+                            return <Ionicons size={size} color={color} name='lock-closed'/>
+                        }
+
+                        return <Ionicons size={size} color={color} name='lock-closed-outline'/>
+                    }
+                }}
+            />
         </Tab.Navigator>
     )
 }
